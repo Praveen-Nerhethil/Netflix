6cf9ef72b687c4b6bc010b69f7c4c9c23be6bb15
Praveen <praveennerthethil96@gmail.com>
2021-09-10 12:04:04 +0530

    initial

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className="banner">
+            <div className='content'>
+                <h1 className='title'>Money Heist</h1>
+                <div className='banner_buttons'>
+                    <button className='button'>Play</button>
+                    <button className='button'>My-List</button>
+                </div>
+                <h1 className='description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque reiciendis neque delectus recusandae officia incidunt, fugiat iure totam odio eaque fugit, quisquam ullam esse, nam error voluptatibus impedit corporis? Id?</h1>
+            </div>
+            <div className="fade">
+                
+            </div>
+        
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/Components/RowPost/RowPost.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function RowPost() {
+    return (
+        <div className='row'>
+            <h2 className='title'>Title</h2>
+            <div className="posters"></div>
+            
+        </div>
+    )
+}
+
+export default RowPost
